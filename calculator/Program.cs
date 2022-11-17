cd1c2e4ae757d6e9b22b565256ab667efb6dc091
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-11-17 00:28:34 +0000

    replに

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,14 +1,32 @@
-﻿Console.WriteLine("数式を入力してね！ 例: 1+23*4/(5-6)");
-
-
-string? input;
-
-do
+﻿
+while (true)
 {
-    input = Console.ReadLine();
-} while (input == null);
+    try
+    {
+
+        Console.Write("数式を入力してね！ 例: 1+23*4/(5-6) >");
+
+        string? input;
+
+        do
+        {
+            input = Console.ReadLine();
+        } while (input == null);
 
 
-Calculator.Lexer lexer = new() { Input = input };
-Calculator.Parser parser = new() { Tokens = lexer.Tokenize().ToArray() };
-Console.WriteLine(parser.Parse().PrettyPrint);
+        Calculator.Lexer lexer = new() { Input = input };
+        Calculator.Parser parser = new() { Tokens = lexer.Tokenize().ToArray() };
+        Calculator.Expr expr = parser.Parse();
+
+        Console.WriteLine(expr.PrettyPrint);
+        Console.WriteLine($"=> {Calculator.Executor.ExecExpr(expr)}");
+
+    }
+    catch (Exception e)
+    {
+
+        Console.WriteLine(e);
+    }
+
+
+}
